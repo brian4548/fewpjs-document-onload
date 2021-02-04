724af253797186cc93f9083f443296eb0ead0160
brian4548 <brianr4548@gmail.com>
2021-02-04 17:09:51 -0600

    commit tree b116f76f1d9ce0f14620845721830264b62b4b72

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    changeTag() 
+})
+
+function changeTag(){
+    let pTag = document.querySelector("#text")
+    pTag.innerHTML = "This is really cool!"
+
+}
